54ffafef1151bf1ea87e0a94b98580781f7b6ddc
William Robson Mendonça <william@tecmasternet.com.br>
2019-07-18 11:26:32 -0300

    Versão funcional

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 700px;
+  margin: 50px auto;
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    align-self: center;
+    align-items: center;
+
+    button {
+      border: 0;
+      background: none;
+    }
+
+    strong {
+      color: #fff;
+      font-size: 24px;
+      margin: 0 15px;
+    }
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 5px;
+    margin-top: 30px;
+  }
+`;
+
+export const Time = styled.li`
+  padding: 10px;
+  border-radius: 4px;
+  background: #fff;
+
+  xopacity: ${props => (props.past ? 0.6 : 1)};
+  background: ${props => (props.past ? '#666' : '#fff')};
+
+  strong {
+    display: block;
+    color: ${props => (props.available ? '#999' : '#7159c1')};
+    font-size: 20px;
+    font-weight: normal;
+  }
+
+  span {
+    display: block;
+    margin-top: 3px;
+    color: ${props => (props.available ? '#999' : '#666')};
+  }
+`;
